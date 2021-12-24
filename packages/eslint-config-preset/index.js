08ee3180cba7c3cbcca8fce8b55403866571e0bf
LING_ZI_QING <onlylingstudio@gmail.com>
2021-12-24 10:20:00 +0800

    fix(preset): 🐛 react version

--- packages/eslint-config-preset/index.js
+++ packages/eslint-config-preset/index.js
@@ -23,24 +23,9 @@ module.exports = {
     node: true,
   },
   // 解决 TypeScript 内置类型 no-undef
-  // eslint-config-react-app  --->  start
   overrides: [
     {
       files: ['**/*.ts?(x)'],
-      parser: '@typescript-eslint/parser',
-      parserOptions: {
-        ecmaVersion: 2018,
-        sourceType: 'module',
-        ecmaFeatures: {
-          jsx: true,
-        },
-
-        // typescript-eslint specific options
-        warnOnUnsupportedTypeScriptVersion: true,
-      },
-      plugins: ['@typescript-eslint'],
-      // If adding a typescript-eslint version of an existing ESLint rule,
-      // make sure to disable the ESLint rule here.
       rules: {
         // TypeScript's `noFallthroughCasesInSwitch` option is more robust (#6906)
         'default-case': 'off',
@@ -48,46 +33,10 @@ module.exports = {
         'no-dupe-class-members': 'off',
         // 'tsc' already handles this (https://github.com/typescript-eslint/typescript-eslint/issues/477)
         'no-undef': 'off',
-
-        // Add TypeScript specific rules (and turn off ESLint equivalents)
-        '@typescript-eslint/consistent-type-assertions': 'warn',
-        'no-array-constructor': 'off',
-        '@typescript-eslint/no-array-constructor': 'warn',
-        'no-redeclare': 'off',
-        '@typescript-eslint/no-redeclare': 'warn',
-        'no-use-before-define': 'off',
-        '@typescript-eslint/no-use-before-define': [
-          'warn',
-          {
-            functions: false,
-            classes: false,
-            variables: false,
-            typedefs: false,
-          },
-        ],
-        'no-unused-expressions': 'off',
-        '@typescript-eslint/no-unused-expressions': [
-          'error',
-          {
-            allowShortCircuit: true,
-            allowTernary: true,
-            allowTaggedTemplates: true,
-          },
-        ],
-        'no-unused-vars': 'off',
-        '@typescript-eslint/no-unused-vars': [
-          'warn',
-          {
-            args: 'none',
-            ignoreRestSiblings: true,
-          },
-        ],
-        'no-useless-constructor': 'off',
-        '@typescript-eslint/no-useless-constructor': 'warn',
+        // 'react/jsx-fragments': ['error', 'syntax'],
       },
     },
   ],
-  // eslint-config-react-app  --->  end
   rules: {
     // 自定义你的规则
     // interface 是一个空，继承其他申明可能不会做扩展
@@ -118,7 +67,7 @@ module.exports = {
   },
   settings: {
     react: {
-      version: 'latest',
+      version: 'detect',
     },
     'import/parsers': {
       // 使用 TypeScript parser
